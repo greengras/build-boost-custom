d4318f942e36dd7b922f79d5fa2a90f8a7715838
greengras <github@homelesstrees.com>
2012-08-04 20:00:35 +0200

    Rakefile for boost 1.50.0 added.

---
+++ rakefile.rb
@@ -0,0 +1,73 @@
+require 'rake'
+require 'rake/clean'
+
+# ==========
+# variables
+# ==========
+
+boost_link   = 'http://sourceforge.net/projects/boost/files/boost/1.50.0/boost_1_50_0.tar.gz'
+
+working_path = Dir.pwd
+build_path   = "#{working_path}/boost_1_50_0"
+
+gcc_path     = '/usr/gcc-4.7.1/bin'
+gcc          = 'gcc-4.7.1'
+gcc_version  = '4.7.1'
+
+config_jam   = 'user-config.jam'
+
+prefix       = '/opt/boost/1.50.0-gcc-4.7.1'
+
+# =============================
+# bootstrap.sh, bjam arguments
+# =============================
+
+bootstrap_options = [
+  "--prefix=#{prefix}",
+  "--with-toolset=gcc"
+]
+
+bjam_options = [
+  "--prefix=#{prefix}",
+  "--layout=tagged",
+  "--user-config=#{config_jam}",
+  "--without-python",
+  "threading=multi",
+  "variant=debug,release",
+  "address-model=32_64",
+  "architecture=x86",
+  "pch=off",
+  "install"
+]
+
+# ============
+# Build boost
+# ============
+
+CLEAN.include build_path
+
+task :default => [ :build, :clean ]
+
+task :build do
+  p "================= start building boost ================="
+
+  sh "wget #{boost_link}"
+  sh "tar xvfz #{boost_link.split('/').last }"
+
+  Dir.chdir build_path
+
+  File.open(config_jam, 'a') do |file|
+    file.write "using gcc : #{gcc_version} : #{gcc_path}/#{gcc} ;\n"
+  end
+
+  sh "./bootstrap.sh #{bargs.join(' ')}"
+  sh "./b2 #{args.join(' ')}"
+
+  Dir.chdir working_path
+
+  sh "rm -Rf #{build_path}"
+
+  p "================= finished building boost ================="
+end
+
+
